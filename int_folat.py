b3c35a51e07a9b14857f22e55682140fd2fae861
rohanravula <109545469+rohanravula@users.noreply.github.com>
2022-07-20 10:26:36 +0530

    Add files via upload

---
+++ int_folat.py
@@ -0,0 +1,46 @@
+'int_statement'
+a=545
+print(a)
+'to_know_the_which_data_type_it_is '
+a=569
+print(a,type(a))
+
+'to_know_the_which_data_type'
+print(type(278))
+print(type(36876))
+print(type(784.8))
+print(type(759.99))
+print(type(749))
+print(type(9700))
+print(type(648.88))
+
+'another_way_to_print_int_vlaue'
+a=6 ; b=5
+print(a)
+print('%d'%b)
+
+'float_statement'
+c=67.98
+# print(c)
+print('%f'%c)
+d=7.8832
+print('%f'%d)
+
+e=34.99
+print('%.1f'%e)
+f=44.56
+print('%.2f'%f)
+print('%.3f'%f)
+
+'''type_conversion_and_casting'''
+
+g=34
+print(type(g))
+print(float(g))
+
+ 
+h=74.56
+print(type(h))
+# print(int(h)) 
+print('%d'%h)
+print('%f'%h)
